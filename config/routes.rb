0a2219cdce91d113385fa159c68f8d9c5316ff43
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-08-27 19:48:56 -0300

    Added authenticated in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root :to => redirect("/users/sign_up")
+  
+  authenticated :user do
+    root to: "pages#home", as: :authenticated_root
+  end
 
   devise_for :users, controllers: {registrations: 'registrations'}
   resources :users, only: [:show, :index]
